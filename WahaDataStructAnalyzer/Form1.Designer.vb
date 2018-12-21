acf4c4527e4a08f33850568018509f8d01475f8d
hiroyukic <hiroyukic386@gmail.com>
2018-12-21 13:37:09 +0900

    start 1337

--- WahaDataStructAnalyzer/Form1.Designer.vb
+++ WahaDataStructAnalyzer/Form1.Designer.vb
@@ -22,10 +22,75 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(484, 41)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(102, 43)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(73, 47)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(389, 19)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(75, 99)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(386, 220)
+        Me.ListBox1.TabIndex = 2
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(80, 367)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(381, 65)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(509, 122)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(82, 16)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(651, 450)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents CheckBox1 As CheckBox
 End Class
--- WahaDataStructAnalyzer/Form1.vb
+++ WahaDataStructAnalyzer/Form1.vb
@@ -1,3 +1,109 @@
 ﻿Public Class Form1
 
+    'カレントディレクトリ
+    Private strCurrentDir As String = System.IO.Directory.GetCurrentDirectory()
+    'For TEST
+    'Private strCurrentDir As String = "D:\Work0\Waha!WTJ_20160405"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' カレントディレクトリを取得する
+        'Dim stCurrentDir As String = System.IO.Directory.GetCurrentDirectory()
+        TextBox1.Text = strCurrentDir
+
+        'ListBox1.ItemHeight = 25
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        '1秒間 Sleep
+        System.Threading.Thread.Sleep(500)
+
+        Dim ret = MsgBox("　処理を開始します。 処理効率はおよそ 3 [file/sec] です。", MessageBoxButtons.OKCancel)
+        Select Case ret
+            Case vbOK
+                ' continue
+            Case vbCancel
+                Return
+        End Select
+
+        '開始時刻
+        Dim timeStampNow As DateTime = System.DateTimeOffset.Now.ToString
+        Dim strTime = String.Format("{0:yyyy-MM-dd HH:mm:ss}", timeStampNow)
+        ListBox1.Items.Add("> " & strTime & "   Start")
+
+        'カーソル（Wait)
+        Me.Cursor = Cursors.WaitCursor
+
+        'カレントディレクトリ以下のファイルをすべて取得する
+        'ワイルドカード"*"は、すべてのファイルを意味する
+        Dim files As String() = System.IO.Directory.GetFiles(strCurrentDir, "*.wtj", System.IO.SearchOption.AllDirectories)
+
+        'プログレスバー初期設定
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = files.Length
+
+        'ListBox1に結果を表示する
+        'ListBox1.Items.AddRange(files)
+        Console.Write(files.Length)
+
+        '出力ファイル名称生成（タイムスタンプ付加）
+        Dim strTimeStampNow = String.Format("{0:yyyyMMdd_HHmm}", timeStampNow)
+        Dim OUTPUT_FILENAME = Me.strCurrentDir & "\wtjStructData_" & strTimeStampNow & ".csv"
+
+        'カレントディレクトリパスの表示
+        ListBox1.Items.Add("[CurrentDir] " & strCurrentDir)
+        ListBox1.Items.Add("")
+        'カレントディレクトリ文字列の長さ
+        Dim iLength As Integer = strCurrentDir.Length()
+
+        'ファイル名称リスト 表示
+        Dim analyser = New SyntacticAnalysis()
+        Dim strFileName = ""
+        For i = 0 To files.Length - 1
+
+            'プログレスバー進捗状況
+            ProgressBar1.Value = i + 1
+            ProgressBar1.Update()
+            ProgressBar1.Invalidate()
+
+            '画面更新
+            Me.Refresh()
+            Application.DoEvents()
+
+            'wtjファイル名取得
+            strFileName = Util.Mid(files(i), iLength + 1)
+            '1行（wtjファイル名）追加
+            ListBox1.Items.Add("[" & i & "] " & strFileName)
+
+            Debug.WriteLine(files(i))
+
+            'WTJデータ構造解析
+            '------------------------------------------------
+            'If Me.CheckBox1.Checked Then
+            '    analyser.Conversion(files(i), OUTPUT_FILENAME, True)
+            'Else
+            '    analyser.Conversion(files(i), OUTPUT_FILENAME)
+            'End If
+            '------------------------------------------------
+            analyser.Conversion(files(i), OUTPUT_FILENAME, Me.CheckBox1.Checked)
+
+        Next
+
+        ListBox1.Items.Add("> " & files.Length & " ファイルを解析しました。")
+        ListBox1.Items.Add("[解析結果] " & OUTPUT_FILENAME)
+
+        '終了時刻
+        timeStampNow = System.DateTimeOffset.Now.ToString
+        strTime = String.Format("{0:yyyy-MM-dd HH:mm:ss}", timeStampNow)
+        ListBox1.Items.Add("> " & strTime & "   End")
+        ListBox1.Items.Add(" ")
+
+        'カーソル（Default)
+        Me.Cursor = Cursors.Default
+
+        'Form1.ActiveForm.BackColor = Color.LightYellow
+
+    End Sub
+
 End Class
+
